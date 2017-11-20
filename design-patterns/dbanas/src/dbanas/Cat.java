60d4a5f29cb51001b2a47469f556c4060c86524c
ousmane-amadou <ousmane.amadou@mail.utoronto.ca>
2017-11-20 17:24:51 -0500

    Commiting previously uncommited data

---
+++ design-patterns/dbanas/src/dbanas/Cat.java
@@ -0,0 +1,10 @@
+package dbanas;
+
+public class Cat extends Animal{
+	
+	public Cat(){
+		super();
+		setSound("Meow");
+	}
+
+}
---
+++ design-patterns/dbanas/src/dbanas/WorkWithAnimals.java
@@ -0,0 +1,8 @@
+package dbanas;
+
+public class WorkWithAnimals {
+	public static void main(String[] args) {
+		Dog fido = new Dog();
+		fido.setName("fido");
+	}
+}
